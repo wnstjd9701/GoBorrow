e0e49989f7f25a7b9092849815f6307539623a7a
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-03-26 20:40:40 +0900

    Lends Project

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { LOGIN_USER, REGISTER_USER } from './action_type.js';
 
 export async function loginUser(dataToSubmit) {
-  const request = await axios.post('/user/login', dataToSubmit).then((response) => response.data);
+  const request = await axios.post('/app/users/login', dataToSubmit).then(response => response.data);
   return {
     type: LOGIN_USER,
     payload: request,
@@ -10,7 +10,7 @@ export async function loginUser(dataToSubmit) {
 }
 
 export async function registerUser(dataToSubmit) {
-  const request = await axios.post('/users/register', dataToSubmit).then((response) => response.data);
+  const request = await axios.post('/app/users/signUp', dataToSubmit).then(response => response.data);
   return {
     type: REGISTER_USER,
     payload: request,
