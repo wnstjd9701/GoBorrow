41bcd1b50b71dc3628ed5d8d621bd750afdfb6d5
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-03-29 12:53:45 +0900

    Lends Project

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -5,7 +5,6 @@ import '../../../App.css';
 export default function Header() {
   const header_style = {
     cursor: 'pointer',
-    width: '200px',
     margin: '0 auto',
     display: 'block',
   };
@@ -14,7 +13,7 @@ export default function Header() {
       <img
         className="App-logo"
         style={header_style}
-        src="https://ufwo.s3.ap-northeast-2.amazonaws.com/%EC%83%98%ED%94%8C%EB%A1%9C%EA%B3%A064.png"
+        src="https://ufwo.s3.ap-northeast-2.amazonaws.com/%EC%83%98%ED%94%8C%EB%A1%9C%EA%B3%A0.png"
         href="https://ufwo.s3.ap-northeast-2.amazonaws.com/%EC%83%98%ED%94%8C%EB%A1%9C%EA%B3%A064.png"
       ></img>
     </header>
