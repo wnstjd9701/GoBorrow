50046d6db45b3689c6676b05b58d02be0f5236e3
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-04-08 23:54:39 +0900

    Lends Project

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -33,7 +33,7 @@ export default function LandingPage(props) {
   };
   const testItem = [
     {
-      cname: '단국대학교',
+      cname: '단국대학교 죽전캠퍼스',
       place: '경기도',
     },
     {
@@ -56,15 +56,23 @@ export default function LandingPage(props) {
     <>
       <Header />
       <div style={{ textAlign: 'center', fontWeight: 700, fontSize: '20px', margin: '50px 0 20px 0' }}>조직/기관명을 검색하세요</div>
-      <Stack style={{ margin: '0 auto' }} spacing={2} sx={{ width: 300 }}>
-        <Autocomplete
-          id="free-solo-demo"
-          freeSolo
-          options={testItem.map(option => option.cname)}
-          renderInput={params => <TextField {...params} onChange={onKeywordHandler} onSubmit={onSearchHandler} label="Search.." />}
-        />
-        <SearchIcon style={{ display: 'inline-block' }} />
-      </Stack>
+      <form onSubmit={onSearchHandler}>
+        <Stack style={{ flexDirection: 'row', margin: '0 auto' }} spacing={2} sx={{ width: 330 }}>
+          <Autocomplete
+            style={{ width: 'inherit' }}
+            id="free-solo-demo"
+            freeSolo
+            options={testItem.map(option => option.cname)}
+            onChange={(e, newValue) => {
+              setKeyword(newValue);
+            }}
+            renderInput={params => <TextField {...params} onChange={onKeywordHandler} label="Search.." />}
+          />
+          <button style={{ margin: '0 auto' }}>
+            <SearchIcon style={{ display: 'inline-block' }} />
+          </button>
+        </Stack>
+      </form>
     </>
   );
 }
