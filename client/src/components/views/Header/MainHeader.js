6380e8ee3846e7579a0ae6dccacdd0fe950bfd3c
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-04-14 21:07:43 +0900

    Update MyInfo Page

--- client/src/components/views/Header/MainHeader.js
+++ client/src/components/views/Header/MainHeader.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { LogoutUser } from '../../../_actions/user_action';
 import authAxios from '../../../lib/refreshToken';
+import { Container } from '@mui/material';
 
 function MainHeader(props) {
   const [name, setName] = useState(null);
@@ -43,34 +44,22 @@ function MainHeader(props) {
     getProfile();
   }, []);
   return (
-    <header style={head_style}>
-      <div stlye={div_style}>
-        <img
-          className=""
-          style={header_style}
-          src="https://s3.ap-northeast-2.amazonaws.com/img.castlejun-2.shop/%EB%A9%94%EC%9D%B8%EB%A1%9C%EA%B3%A0.png"
-          href="https://s3.ap-northeast-2.amazonaws.com/img.castlejun-2.shop/%EB%A9%94%EC%9D%B8%EB%A1%9C%EA%B3%A0.png"
-          alt="header"
-        ></img>
-      </div>
-      <div
-        style={{
-          flexGrow: 1,
-        }}
-      >
-        <Button
+    <Container maxWidth="xl">
+      <header style={head_style}>
+        <div stlye={div_style}>
+          <img
+            className=""
+            style={header_style}
+            src="https://s3.ap-northeast-2.amazonaws.com/img.castlejun-2.shop/%EB%A9%94%EC%9D%B8%EB%A1%9C%EA%B3%A0.png"
+            href="https://s3.ap-northeast-2.amazonaws.com/img.castlejun-2.shop/%EB%A9%94%EC%9D%B8%EB%A1%9C%EA%B3%A0.png"
+            alt="header"
+          ></img>
+        </div>
+        <div
           style={{
-            color: 'black',
-            float: 'right',
-            marginRight: '50px',
+            flexGrow: 1,
           }}
-          size="medium"
-          variant="text"
-          onClick={postLogOut}
         >
-          LOGOUT
-        </Button>
-        <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
           <Button
             style={{
               color: 'black',
@@ -78,18 +67,31 @@ function MainHeader(props) {
             }}
             size="medium"
             variant="text"
+            onClick={postLogOut}
           >
-            HOME
+            LOGOUT
           </Button>
-        </Link>
-        <span style={{ float: 'right', display: 'inline-flex', lineHeight: '1.75em', padding: '6px 8px' }}>
-          <Link to="/users/myPage" style={{ textDecoration: 'none', color: 'green' }}>
-            <b>{name}</b>
+          <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
+            <Button
+              style={{
+                color: 'black',
+                float: 'right',
+              }}
+              size="medium"
+              variant="text"
+            >
+              HOME
+            </Button>
           </Link>
-          님 환영합니다!
-        </span>
-      </div>
-    </header>
+          <span style={{ float: 'right', display: 'inline-flex', lineHeight: '1.75em', padding: '6px 8px' }}>
+            <Link to="/users/myPage" style={{ textDecoration: 'none', color: 'green' }}>
+              <b>{name}</b>
+            </Link>
+            님 환영합니다!
+          </span>
+        </div>
+      </header>
+    </Container>
   );
 }
 
