fdb315d96b05ef0e61a8e08e6499bb0f1d3b16f8
wnstjd9701 <wnstjd9701@naver.com>
2022-05-23 17:39:26 +0900

    Update: OrganizationRouter

--- server/src/app/organization/organizationRoute.js
+++ server/src/app/organization/organizationRoute.js
@@ -6,5 +6,6 @@ import organizationController from './organizationController.js';
 
 router.get('/', organizationController.organizationInformation); // 기관 검색 API
 router.get('/:organizationName', organizationController.organizationDetail); // 기관 정보 API
-router.get('/:productId'); // 기관 대여 물품
+router.get('/:productId', organizationController.organizationProductInfo); // 물품 대여 API
+router.get('/:productId');
 export default router;
