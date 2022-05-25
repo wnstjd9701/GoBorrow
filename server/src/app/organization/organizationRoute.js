1f5433cef4936f6e9c36bcf05eb189b51d3c52ae
wnstjd9701 <wnstjd9701@naver.com>
2022-05-25 17:53:22 +0900

    Update: Rent Product

--- server/src/app/organization/organizationRoute.js
+++ server/src/app/organization/organizationRoute.js
@@ -6,6 +6,7 @@ import organizationController from './organizationController.js';
 
 router.get('/', organizationController.organizationInformation); // 기관 검색 API
 router.get('/:organizationName', organizationController.organizationDetail); // 기관 정보 API
-router.get('/:organizationName/:productId', organizationController.organizationProductInfo); // 대여 물품 리스트 API
+router.get('/:organizationName/:productId', authentication, organizationController.organizationProductInfo); // 대여 물품 리스트 API
+router.post('/:organizationName/:productId', authentication, organizationController.productReservation); // 물품 예약
 //router.get('/org', organizationController.organizationMain); // 기관 관리 페이지 API
 export default router;
