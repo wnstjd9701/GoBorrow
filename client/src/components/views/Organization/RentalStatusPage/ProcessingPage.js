1d08c1753171ba2f7da7952728a523655e756d85
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-05-27 15:06:17 +0900

    Add Rental Status Page

---
+++ client/src/components/views/Organization/RentalStatusPage/ProcessingPage.js
@@ -0,0 +1,352 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Header from '../../Public/Header/MainHeader';
+import Footer from '../../Public/Footer/Footer';
+import Sidebar from '../MainPage/SideBar/Sidebar';
+import { useTheme } from '@mui/material/styles';
+import { Avatar, Button, CardActions, CardContent, Divider, Grid, Menu, MenuItem, Typography, Box } from '@mui/material';
+import MainCard from '../MainPage/Customization/Cards/MainCard';
+import SkeletonPopularCard from '../MainPage/Customization/Cards/PopularCard';
+import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
+import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+import CheckBoxRoundedIcon from '@mui/icons-material/CheckBoxRounded';
+
+const RentalProcessingPage = ({ isLoading }) => {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const leftDrawerOpened = useSelector(state => state.customization.opened);
+
+  const handleLeftDrawerToggle = () => {
+    dispatch({ type: '@customization/SET_MENU', opened: !leftDrawerOpened });
+  };
+  return (
+    <>
+      {isLoading ? (
+        <SkeletonPopularCard />
+      ) : (
+        <>
+          <Header />
+          <Box sx={{ display: 'flex' }}>
+            <Sidebar drawerOpen={leftDrawerOpened} drawerToggle={handleLeftDrawerToggle} />
+            <MainCard sx={{ border: '1px solid rgba(144, 202, 249, 0.46)', borderRadius: '10px', padding: '5px' }} content={false}>
+              <CardContent>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Grid container alignContent="center" justifyContent="space-between">
+                      <Grid item>
+                        <Typography variant="h5">Recent Request</Typography>
+                      </Grid>
+                      <Grid item>
+                        <MoreHorizOutlinedIcon
+                          fontSize="small"
+                          sx={{
+                            color: theme.palette.primary[200],
+                            cursor: 'pointer',
+                          }}
+                          aria-controls="menu-popular-card"
+                          aria-haspopup="true"
+                          onClick={handleClick}
+                        />
+                        <Menu
+                          id="menu-popular-card"
+                          anchorEl={anchorEl}
+                          keepMounted
+                          open={Boolean(anchorEl)}
+                          onClose={handleClose}
+                          variant="selectedMenu"
+                          anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                          }}
+                          transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                          }}
+                        >
+                          <MenuItem onClick={handleClose}> Today</MenuItem>
+                          <MenuItem onClick={handleClose}> This Month</MenuItem>
+                          <MenuItem onClick={handleClose}> This Year </MenuItem>
+                        </Menu>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Grid container direction="column">
+                      <Grid item>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                          <Grid item>
+                            <Typography variant="subtitle1" color="inherit">
+                              우산
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Grid container alignItems="center" justifyContent="space-between">
+                              <Grid item>
+                                <Typography variant="subtitle1" color="inherit">
+                                  32173058 이성준
+                                </Typography>
+                              </Grid>
+                              <Grid item>
+                                <Avatar
+                                  variant="rounded"
+                                  sx={{
+                                    width: 16,
+                                    height: 16,
+                                    backgroundColor: 'black',
+                                    color: theme.palette.success.light,
+                                    marginLeft: 1.875,
+                                  }}
+                                >
+                                  <CheckBoxRoundedIcon fontSize="medium" color="inherit" />
+                                </Avatar>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                      <Grid item>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                          <Grid item>
+                            <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
+                              UR1022
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
+                              2022-05-20 ~ 2022-05-21
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Divider sx={{ my: 1.5 }} />
+                    <Grid container direction="column">
+                      <Grid item>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                          <Grid item>
+                            <Typography variant="subtitle1" color="inherit">
+                              우산
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Grid container alignItems="center" justifyContent="space-between">
+                              <Grid item>
+                                <Typography variant="subtitle1" color="inherit">
+                                  32194747 최지윤
+                                </Typography>
+                              </Grid>
+                              <Grid item>
+                                <Avatar
+                                  variant="rounded"
+                                  sx={{
+                                    width: 16,
+                                    height: 16,
+                                    backgroundColor: 'black',
+                                    color: theme.palette.success.light,
+                                    marginLeft: 1.875,
+                                  }}
+                                >
+                                  <CheckBoxRoundedIcon fontSize="medium" color="inherit" />
+                                </Avatar>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                      <Grid item>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                          <Grid item>
+                            <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
+                              UR1023
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
+                              2022-05-22 ~ 2022-05-23
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Divider sx={{ my: 1.5 }} />
+                    <Grid container direction="column">
+                      <Grid item>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                          <Grid item>
+                            <Typography variant="subtitle1" color="inherit">
+                              아이패드
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Grid container alignItems="center" justifyContent="space-between">
+                              <Grid item>
+                                <Typography variant="subtitle1" color="inherit">
+                                  32174727 최한윤
+                                </Typography>
+                              </Grid>
+                              <Grid item>
+                                <Avatar
+                                  variant="rounded"
+                                  sx={{
+                                    width: 16,
+                                    height: 16,
+                                    backgroundColor: 'black',
+                                    color: theme.palette.success.light,
+                                    marginLeft: 1.875,
+                                  }}
+                                >
+                                  <CheckBoxRoundedIcon fontSize="medium" color="inherit" />
+                                </Avatar>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                      <Grid item>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                          <Grid item>
+                            <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
+                              IP4255
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
+                              2022-05-22 ~ 2022-05-29
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Divider sx={{ my: 1.5 }} />
+                    <Grid container direction="column">
+                      <Grid item>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                          <Grid item>
+                            <Typography variant="subtitle1" color="inherit">
+                              자전거
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Grid container alignItems="center" justifyContent="space-between">
+                              <Grid item>
+                                <Typography variant="subtitle1" color="inherit">
+                                  32194747 최지윤
+                                </Typography>
+                              </Grid>
+                              <Grid item>
+                                <Avatar
+                                  variant="rounded"
+                                  sx={{
+                                    width: 16,
+                                    height: 16,
+                                    backgroundColor: 'black',
+                                    color: theme.palette.success.light,
+                                    marginLeft: 1.875,
+                                  }}
+                                >
+                                  <CheckBoxRoundedIcon fontSize="medium" color="inherit" />
+                                </Avatar>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                      <Grid item>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                          <Grid item>
+                            <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
+                              BK7777
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
+                              2022-05-24 ~ 2022-05-28
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Divider sx={{ my: 1.5 }} />
+                    <Grid container direction="column">
+                      <Grid item>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                          <Grid item>
+                            <Typography variant="subtitle1" color="inherit">
+                              노트북
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Grid container alignItems="center" justifyContent="space-between">
+                              <Grid item>
+                                <Typography variant="subtitle1" color="inherit">
+                                  32162956 윤준성
+                                </Typography>
+                              </Grid>
+                              <Grid item>
+                                <Avatar
+                                  variant="rounded"
+                                  sx={{
+                                    width: 16,
+                                    height: 16,
+                                    backgroundColor: 'black',
+                                    color: theme.palette.success.light,
+                                    marginLeft: 1.875,
+                                  }}
+                                >
+                                  <CheckBoxRoundedIcon fontSize="medium" color="inherit" />
+                                </Avatar>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                      <Grid item>
+                        <Grid container alignItems="center" justifyContent="space-between">
+                          <Grid item>
+                            <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
+                              NB1004
+                            </Typography>
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="subtitle2" sx={{ color: 'success.dark' }}>
+                              2022-05-22 ~ 2022-05-25
+                            </Typography>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </CardContent>
+              <CardActions sx={{ p: 1.25, pt: 0, justifyContent: 'center' }}>
+                <Button size="small" disableElevation>
+                  View All
+                  <ChevronRightOutlinedIcon />
+                </Button>
+              </CardActions>
+            </MainCard>
+          </Box>
+          <Footer />
+        </>
+      )}
+    </>
+  );
+};
+
+RentalProcessingPage.propTypes = {
+  isLoading: PropTypes.bool,
+};
+
+export default RentalProcessingPage;
