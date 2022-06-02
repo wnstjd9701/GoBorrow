51cb67640d5782c0cf6726d3d6ebe3b55b156315
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-06-02 18:06:51 +0900

    Add Menu Button in Organization Page Header

--- client/src/components/views/Public/Header/MainHeader.js
+++ client/src/components/views/Public/Header/MainHeader.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { LogoutUser } from '../../../../_actions/user_action';
+import { SET_MENU } from '../../../../_actions/action_type';
 import authAxios from '../../../../lib/refreshToken';
+import { Avatar } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { IconMenu2 } from '@tabler/icons';
 
-function MainHeader(props) {
+function MainHeader({ org }) {
+  const theme = useTheme();
   const [name, setName] = useState(null);
   const dispatch = useDispatch();
   let navigate = useNavigate();
@@ -23,6 +28,7 @@ function MainHeader(props) {
     display: 'flex',
     padding: '0 40px',
   };
+
   const postLogOut = e => {
     dispatch(LogoutUser()).then(response => {
       if (response.payload.isSuccess) {
@@ -34,6 +40,12 @@ function MainHeader(props) {
     });
   };
 
+  const leftDrawerOpened = useSelector(state => state.customization.opened);
+
+  const handleLeftDrawerToggle = () => {
+    dispatch({ type: SET_MENU, opened: !leftDrawerOpened });
+  };
+
   useEffect(() => {
     async function getProfile() {
       const profile = await authAxios.get('/app/users/profile');
@@ -43,6 +55,7 @@ function MainHeader(props) {
     }
     getProfile();
   }, []);
+
   return (
     <header style={head_style}>
       <div stlye={div_style}>
@@ -54,6 +67,36 @@ function MainHeader(props) {
           alt="header"
         ></img>
       </div>
+      {org ? (
+        <Avatar
+          variant="rounded"
+          sx={{
+            ...theme.typography.commonAvatar,
+            ...theme.typography.mediumAvatar,
+            transition: 'all .2s ease-in-out',
+            background: theme.palette.success.light,
+            color: theme.palette.success.dark,
+            width: '32px',
+            height: '32px',
+            '&:hover': {
+              background: theme.palette.success.dark,
+              color: theme.palette.success.light,
+            },
+            [theme.breakpoints.down('md')]: {
+              width: '28px',
+              height: '28px',
+            },
+            [theme.breakpoints.down('sm')]: {
+              width: '24px',
+              height: '24px',
+            },
+          }}
+          onClick={handleLeftDrawerToggle}
+          color="inherit"
+        >
+          <IconMenu2 stroke={1.5} size="1.3rem" />
+        </Avatar>
+      ) : null}
       <div
         style={{
           flexGrow: 1,
